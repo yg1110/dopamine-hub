d129cf17c3f00e5b477db2935cb3c60b4e775372
yg1110 <younggil9488@gmail.com>
2024-12-20 23:35:00 +0900

    eslint, prettier 설정 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import './globals.css';
+
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "dopamine-hub",
-  description: "dopamine-hub",
+  title: 'dopamine-hub',
+  description: 'dopamine-hub',
 };
 
 export default function RootLayout({
